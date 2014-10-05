b6ff0a228462f969500f49a8f56df1cb4fdccdf8
Thomas Preece <t.preece@warwick.ac.uk>
2014-10-05 13:17:57 +0100

    Initial Commit

---
+++ Search.php
@@ -0,0 +1,225 @@
+<?php		
+include("./../Includes/HeaderFunctions.php");
+
+//$Content	0 - Error
+//			1 - PHP
+//			2 - JSON	
+
+if(isset($_GET['ContentType'])){
+	if ($_GET['ContentType']=="JSON"){
+		$Content = 2;
+	}elseif($_GET['ContentType']=="XML"){
+		$Content = 1;
+	}else{
+		$Content = 0;
+	}
+}else{
+	$Content = 0;
+}
+
+if($Content == 1){
+	header('Content-type: text/xml');
+	echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
+<Data>";	
+}elseif($Content==2){
+	header('Content-Type: application/json');
+	echo "{
+	\"games\": [
+	";
+}else{
+	echo "You must specify content type!<br/>
+	Please see <a href='http://tfgdb.com/API/'>http://tfgdb.com/API/</a>";
+	die;
+}
+		
+$SearchLine = "WHERE";
+
+$_CLEANREQUEST = array();
+
+foreach($_GET as $key => $value){
+	$_CLEANREQUEST[addslashes(htmlspecialchars($key,ENT_QUOTES))] = addslashes(htmlspecialchars($value,ENT_QUOTES));
+}
+
+if (isset($_CLEANREQUEST["Mode"]) && $_CLEANREQUEST["Mode"] != ""){
+	$SearchLine = $SearchLine." ModeBITS & ".intval($_CLEANREQUEST["Mode"])." AND";
+}
+
+if (isset($_CLEANREQUEST["Gen"]) && $_CLEANREQUEST["Gen"] != ""){
+	$SearchLine = $SearchLine." GenreBITS & ".intval($_CLEANREQUEST["Gen"])." AND";
+}
+
+if (isset($_CLEANREQUEST["Plat"]) && $_CLEANREQUEST["Plat"] != ""){
+	$SearchLine = $SearchLine." PlatformBITS & ".intval($_CLEANREQUEST["Plat"])." AND";
+}
+
+if (isset($_CLEANREQUEST["Release"]) && $_CLEANREQUEST["Release"] != ""){
+	foreach($GAMERELEASE as $temp){
+		if($temp['ID']==$_CLEANREQUEST["Release"]){
+			$SearchLine = $SearchLine." `Release`='".$_CLEANREQUEST["Release"]."' AND";
+		}
+	}
+}						
+
+if (isset($_CLEANREQUEST["Letter"]) && $_CLEANREQUEST["Letter"] != ""){
+	$_CLEANREQUEST['Search'] = "";
+	$SearchLine = $SearchLine." Name LIKE '".$_CLEANREQUEST["Letter"]."%' AND";
+}else{					
+	if (isset($_CLEANREQUEST["Search"])  && $_CLEANREQUEST["Search"] != "" && strlen($_CLEANREQUEST["Search"])>1){
+			$SearchLine = $SearchLine." (Name LIKE '%".$_CLEANREQUEST["Search"]."%' OR About LIKE '%".$_CLEANREQUEST["Search"]."%') AND";
+	}else{
+
+	}
+}
+
+if (isset($_CLEANREQUEST["Graphics"])  && $_CLEANREQUEST["Graphics"] != ""){
+	
+	foreach($GAMEGRAPHICS as $temp){
+		if($temp['ID']==$_CLEANREQUEST["Graphics"]){
+			$SearchLine = $SearchLine." Graphics='".$_CLEANREQUEST["Graphics"]."' AND";
+		}
+	}			
+}
+
+if (isset($_CLEANREQUEST["Type"])  && $_CLEANREQUEST["Type"] != ""){
+	
+	foreach($GAMETYPES as $temp){
+		if($temp['ID']==$_CLEANREQUEST["Type"]){
+			
+			$SearchLine = $SearchLine." Type='".$_CLEANREQUEST["Type"]."' AND";
+		}
+	}				
+
+}
+
+if (isset($_CLEANREQUEST["Source"]) && $_CLEANREQUEST["Source"] != ""){
+	
+	foreach($GAMESOURCES as $temp){
+		if($temp['ID']==$_CLEANREQUEST["Source"]){
+			
+			$SearchLine = $SearchLine." Source='".$_CLEANREQUEST["Source"]."' AND";
+		}
+	}			
+
+}
+
+
+
+if (isset($_CLEANREQUEST["Studio"]) && $_CLEANREQUEST["Studio"] != ""){
+	
+	foreach($GAMESTUDIOS as $temp){
+		if($temp['ID']==$_CLEANREQUEST["Studio"]){
+			$SearchLine = $SearchLine." Studio='".$_CLEANREQUEST["Studio"]."' AND";
+		}
+	}						
+	
+}	
+
+$SearchLine = $SearchLine." `QuedTodaysGame`=0";						
+		
+if (isset($_CLEANREQUEST["Sort"])){
+
+	
+	if($_CLEANREQUEST["Sort"]==1){
+		$SearchLine = $SearchLine." ORDER BY Rating, RateNum ";							
+	}elseif($_CLEANREQUEST["Sort"]==2){
+		$SearchLine = $SearchLine." ORDER BY Rating DESC, RateNum DESC ";
+	}elseif($_CLEANREQUEST["Sort"]==3){
+		$SearchLine = $SearchLine." ORDER BY ID DESC ";								
+	}else{
+		$SearchLine = $SearchLine." ORDER BY Name ";			
+	}
+}else{
+	$SearchLine = $SearchLine." ORDER BY Name ";
+}
+
+$GameSearch = $Gamedb->prepare("SELECT * FROM freegames ".$SearchLine);
+$GameSearch->execute();
+
+while($row = $GameSearch->fetch(PDO::FETCH_ASSOC)){
+
+	$Desc = $row['About'];
+	$Desc = preg_replace('/<br\/>/',"\n",$Desc);
+	$Desc = preg_replace('/[^a-zA-Z0-9\., \n\'\"’\(\)&\?!\+-:;]/',"",$Desc);
+	$Desc = preg_replace('/[\n]/',"<br/>",$Desc);
+	$Desc = preg_replace('/[&][^#]/',"&amp; ",$Desc);	
+	
+	if(strlen($Desc) > 340){
+		$Desc = substr($Desc,0,340)."... ";
+	}	
+
+	if($Content == 1){
+		echo "
+	<Game>
+		<ID>".$row['ID']."</ID>
+		<Name>".$row['Name']."</Name>
+		<About>".$Desc."</About>	
+	</Game>";
+	}elseif($Content == 2){
+		if($FirstDone==true){
+			echo ",";
+		}
+
+		echo "
+		{
+			\"id\": \"".$row['ID']."\",
+			\"name\": \"".$row['Name']."\",
+			\"about\": \"".$Desc."\"
+		}
+		";
+
+		$FirstDone = true;
+	} 
+}
+
+if($Content == 1){
+	echo "
+</Data>";
+}elseif($Content == 2){
+	echo "]
+	}";
+}	
+				
+?>
+
+<!--
+
+
+$ModeArray = array();
+$GenreArray = array();
+$PlatformArray = array();
+foreach($_CLEANREQUEST as $key => $value){
+	if($value=='on'){
+		if(substr($key,0,4)=='Mode'){
+			$ModeArray[] = substr($key,4);
+		}elseif(substr($key,0,3)=='Gen'){
+			$GenreArray[] = substr($key,3);
+		}elseif(substr($key,0,4)=='Plat'){
+			$PlatformArray[] = substr($key,4);
+		}				
+	}
+}
+
+$ModeLine = "";
+foreach($ModeArray as $temp){
+	$ModeLine = $ModeLine." ModeBITS & ".$temp." OR";
+}
+if($ModeLine!=""){
+	$SearchLine = $SearchLine."(".substr($ModeLine,0,-3).") AND";
+}
+
+$GenreLine = "";
+foreach($GenreArray as $temp){
+	$GenreLine = $GenreLine." GenreBITS & ".$temp." OR";
+}
+if($GenreLine!=""){
+	$SearchLine = $SearchLine."(".substr($GenreLine,0,-3).") AND";
+}		
+
+$PlatformLine = "";
+foreach($PlatformArray as $temp){
+	$PlatformLine = $PlatformLine." PlatformBITS & ".$temp." OR";
+}
+if($PlatformLine!=""){
+	$SearchLine = $SearchLine."(".substr($PlatformLine,0,-3).") AND";
+}	
+-->
